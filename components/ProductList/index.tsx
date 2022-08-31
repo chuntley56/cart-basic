1659502037aa4f182a4051b8568a7b6369284e0c
Corey Huntley <corey.huntley@dollarshaveclub.com>
2022-08-31 14:55:20 -0700

    remove component abstraction

--- components/ProductList/index.tsx
+++ components/ProductList/index.tsx
@@ -1,20 +1,32 @@
 import React, { FunctionComponent, useState } from 'react'
 import { Product } from '../../types'
-import { ProductCard } from '../ProductCard'
+import Image from 'next/image'
 
 export type ProductListType = {
    products: Product[]
 }
 
-  
-
 export const ProductList: FunctionComponent<ProductListType> = ({ products }) => {
     const [cart, setCart] = useState<string[]>([])
-    console.log({cart})
+    console.log({ cart })
 
     return (
         <div style={{display: 'flex', gap: '50px', justifyContent: 'center'}}>
-            {products.map((product: any, i) => <ProductCard product={product} key={i} onClick={() => setCart(cart => [...cart, product.sku])} />)
+            {products.map((product: any, i) => {
+                const {name, price, sku} = product
+                return (
+                    <div key={i}  style={{backgroundColor: '#eee', textAlign: 'center'}}>
+                        <h3>
+                            {name}
+                        </h3>
+                        <Image alt={name} height={200} width={200} src={`https://dsc-assets.imgix.net/images/product-images/product-tile/${sku}.png`} />
+                        <p>
+                            ${price}
+                        </p>
+                        <button onClick={() => setCart(cart => [...cart, product.sku])}>Add to cart</button>
+                    </div>
+                )
+            })
             }
         </div>
     )
